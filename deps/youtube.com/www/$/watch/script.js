79d7279a055ce0e411dc25a5650a715b1d57f440
leonhma <67312077+leonhma@users.noreply.github.com>
2021-03-20 13:44:25 +0100

    Create script.js
    automatically skip ads

---
+++ deps/youtube.com/www/$/watch/script.js
@@ -0,0 +1,22 @@
+function wait(selector, cb) {
+  const el = document.querySelector(selector);
+  if(el) {
+    cb(el)
+    return
+  }
+  window.requestAnimationFrame(() => {
+    wait(selector, cb)
+  })
+}
+
+var observer = new MutationObserver(() => {
+  console.log('tick')
+  const el = document.querySelector('.ytp-ad-skip-button.ytp-button')
+  if(el) {
+    el.click();
+  }
+})
+
+wait('.video-ads', (el) => {
+  observer.observe(el, {attributes: true, subtree: true, childList: true})
+})
